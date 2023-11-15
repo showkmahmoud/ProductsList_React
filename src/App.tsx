3cf176c6f491ef2282baf1abaf5601b440eee448
showk mahmoud <showkmahmoud@gmail.com>
2023-11-15 23:58:09 +0200

    NF: basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Navbar from "./core/nav/nav";
+import home from "./pages/home/home";
+import admin from "./pages/admin/admin";
+import user from "./pages/user/user";
+import notFound from "./pages/not-found/notFound";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route index Component={home} />
+        <Route path="/admin" Component={admin} />
+        <Route path="/user" Component={user} />
+        <Route path="*" Component={notFound} />
+      </Routes>
+    </Router>
   );
 }
 
